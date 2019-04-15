cebcc73f806d40cff6f52863ba64977f47b2d75f
ABREU ARNAUD <aabreu@unistra.fr>
2019-04-15 14:15:27 +0200

    commented pyplot lines uncompatible with gpu

--- pysliderois/tissue.py
+++ pysliderois/tissue.py
@@ -148,9 +148,9 @@ def naive_patch_placement_optimization(mask, psize, verbose=False):
     # debug
     if verbose:
         patchposmask = numpy.logical_and(patchposmask, eroded)
-        plt.figure(figsize=(10, 10), dpi=300)
-        plt.imshow(displaymask + dilation(patchposmask).astype(int))
-        plt.show()
+        # plt.figure(figsize=(10, 10), dpi=300)
+        # plt.imshow(displaymask + dilation(patchposmask).astype(int))
+        # plt.show()
 
     return numpy.array(finali), numpy.array(finalj)
 
